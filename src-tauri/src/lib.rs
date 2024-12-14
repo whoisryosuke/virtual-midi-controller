1e90b151f675dc4e154dac4bad2c5ee81dc6694d
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-12-13 16:02:10 -0800

    :bug: unwrap causing errors when playing note

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ struct AppData {
 
 #[tauri::command]
 fn midi_connect_to_port(state: State<'_, Mutex<AppData>>) {
+    println!("[MIDI] Connecting to MIDI port");
     let midi_out = MidiOutput::new("My Test Output").unwrap();
     let mut state = state.lock().unwrap();
     println!("\nOpening connection");
@@ -25,49 +26,69 @@ fn midi_connect_to_port(state: State<'_, Mutex<AppData>>) {
 
 #[tauri::command]
 fn midi_play_note(state: State<'_, Mutex<AppData>>, note: u8) {
+    println!("[MIDI] Playing note");
     let mut state = state.lock().unwrap();
-    let connection = state.connection.as_mut().unwrap();
+    let check_connection = state.connection.as_mut();
 
-    // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-    let mut play_note = |note: u8, duration: u64| {
-        const NOTE_ON_MSG: u8 = 0x90;
-        const NOTE_OFF_MSG: u8 = 0x80;
-        const VELOCITY: u8 = 0x64;
-        // We're ignoring errors in here
-        let _ = connection.send(&[NOTE_ON_MSG, note, VELOCITY]);
-        sleep(Duration::from_millis(duration * 150));
-        let _ = connection.send(&[NOTE_OFF_MSG, note, VELOCITY]);
-    };
+    match check_connection {
+        Some(connection) => {
+            // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
+            let mut play_note = |note: u8, duration: u64| {
+                const NOTE_ON_MSG: u8 = 0x90;
+                const NOTE_OFF_MSG: u8 = 0x80;
+                const VELOCITY: u8 = 0x64;
+                // We're ignoring errors in here
+                let _ = connection.send(&[NOTE_ON_MSG, note, VELOCITY]);
+                sleep(Duration::from_millis(duration * 150));
+                let _ = connection.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+            };
 
-    play_note(note, 4);
+            play_note(note, 4);
+        },
+        None => println!("[MIDI] Error! Need to select a port first!"),
+    }
 }
 
 #[tauri::command]
 fn midi_play_note_press(state: State<'_, Mutex<AppData>>, note: u8) {
+    println!("[MIDI] Pressing note");
     let mut state = state.lock().unwrap();
-    let connection = state.connection.as_mut().unwrap();
+    let check_connection = state.connection.as_mut();
+
+    match check_connection {
+        Some(connection) => {
 
     const NOTE_ON_MSG: u8 = 0x90;
     const VELOCITY: u8 = 0x64;
     // We're ignoring errors in here
     let _ = connection.send(&[NOTE_ON_MSG, note, VELOCITY]);
+        },
+        None => println!("[MIDI] Error! Need to select a port first!"),
+    }
 
 }
 
 #[tauri::command]
 fn midi_play_note_release(state: State<'_, Mutex<AppData>>, note: u8) {
+    println!("[MIDI] Releasing note");
     let mut state = state.lock().unwrap();
-    let connection = state.connection.as_mut().unwrap();
+    let check_connection = state.connection.as_mut();
 
-    const NOTE_OFF_MSG: u8 = 0x80;
-    const VELOCITY: u8 = 0x64;
-    // We're ignoring errors in here
-    let _ = connection.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+    match check_connection {
+        Some(connection) => {
+            const NOTE_OFF_MSG: u8 = 0x80;
+            const VELOCITY: u8 = 0x64;
+            // We're ignoring errors in here
+            let _ = connection.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+        },
+        None => println!("[MIDI] Error! Need to select a port first!"),
+    }
 
 }
 
 #[tauri::command]
 fn midi_get_ports(state: State<'_, Mutex<AppData>>) -> Vec<String> {
+    println!("[MIDI] Getting port for UI");
     let state = state.lock().unwrap();
     let ports = state.midi_ports.clone();
     let port_names = ports.iter().map(|port| state.midi_channel.port_name(port).unwrap()).collect::<Vec<_>>();
@@ -78,6 +99,7 @@ fn midi_get_ports(state: State<'_, Mutex<AppData>>) -> Vec<String> {
 
 #[tauri::command]
 fn midi_set_port(state: State<'_, Mutex<AppData>>, index: usize) {
+    println!("[MIDI] Setting port");
     let mut state = state.lock().unwrap();
     state.current_port = state.midi_ports
                 .get(index).cloned();
@@ -87,6 +109,7 @@ fn midi_set_port(state: State<'_, Mutex<AppData>>, index: usize) {
 pub fn run() {
     Builder::default()
         .setup(|app| {
+            println!("[MIDI] Hydrating app with ports");
             // We create the MIDI class here to hydrate state with available ports
             let midi_out = MidiOutput::new("My Test Output").unwrap();
             let midi_ports = midi_out.ports();
